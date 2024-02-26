4460adb0ca67c9201870f11e8630ff181c3c296a
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2024-02-26 14:09:56 +0200

    fix

--- controllers/products/newProducts.ts
+++ controllers/products/newProducts.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 import { Product } from '../../models/products';
+import ctrlWrapper from '../../utils/ctrlWrapper';
 
 const getNewProducts = async (req: Request, res: Response) => {
   const products = await Product.find().sort({ createdAt: -1 }).limit(15);
   res.status(200).json(products);
 };
 
-export default getNewProducts;
+export default ctrlWrapper(getNewProducts);
