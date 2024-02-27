7d1bc4dda89b2a4e614bd1a3bff8ea11845e979e
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2024-02-27 13:25:42 +0200

    fix
    Fixed the hardcode of limit, page and skip

--- controllers/products/newProducts.ts
+++ controllers/products/newProducts.ts
@@ -3,7 +3,14 @@ import { Product } from '../../models/products';
 import ctrlWrapper from '../../utils/ctrlWrapper';
 
 const getNewProducts = async (req: Request, res: Response) => {
-  const products = await Product.find().sort({ createdAt: -1 }).limit(15);
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.limit as string) || 15;
+  const skip = (page - 1) * limit;
+
+  const products = await Product.find()
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit);
   res.status(200).json(products);
 };
 
--- controllers/products/salesProducts.ts
+++ controllers/products/salesProducts.ts
@@ -3,9 +3,14 @@ import { Product } from '../../models/products';
 import ctrlWrapper from '../../utils/ctrlWrapper';
 
 const getProductsOnSale = async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.limit as string) || 15;
+  const skip = (page - 1) * limit;
+
   const products = await Product.find({ sale: { $gt: 0 } })
     .sort({ sale: -1 })
-    .limit(15);
+    .skip(skip)
+    .limit(limit);
   res.status(200).json(products);
 };
 
